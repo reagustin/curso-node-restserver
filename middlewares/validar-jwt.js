68c23fdfacaf55930965f761b7c2d53878f79e1d
Agustin <re_agustin@hotmail.com>
2023-01-04 21:00:10 -0300

    Fin sección 10 - version 3.0.0

---
+++ middlewares/validar-jwt.js
@@ -0,0 +1,48 @@
+const { request, response } = require('express');
+const jwt = require('jsonwebtoken');
+
+const Usuario = require('../models/usuario');
+
+const validarJWT = async(req = request, res = response, next) => {
+    const token = req.header('x-token')
+
+    if (!token) {
+        return res.status(401).json({
+            msg: 'No hay token en la peticion'
+        })
+    }
+    try {        
+        const {uid} = jwt.verify(token, process.env.SECRETORPRIVATEKYE);
+        
+        // leer el usuario que corresponde al uid... acorde al modelo
+        const usuario = await Usuario.findById(uid);
+
+        if(!usuario){
+            return res.status(401).json({
+                msg: 'Token no valido - usuario no existe en BD'
+            })
+        }
+        
+        // verificar si el uid tierne estado true
+        if(!usuario.estado){
+            return res.status(401).json({
+                msg: 'Token no valido - usuario con estado: false'
+            })
+        }
+
+        req.usuario = usuario;
+        next();
+    } catch (error) {
+        console.log(error);
+        res.status(401).json({
+            msg: 'Token no valido'
+        })
+    }
+
+
+    console.log(token);
+}
+
+module.exports = {
+    validarJWT
+}
